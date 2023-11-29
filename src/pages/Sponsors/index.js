61399a6fe3afc8b2b7f5a520058d11372ac688e8
Sebastian <sebastianipl0131@gmail.com>
2023-11-29 18:20:12 -0400

    creando boton donacion y traduciendo sponsor-us

--- src/pages/Sponsors/index.js
+++ src/pages/Sponsors/index.js
@@ -1,14 +1,14 @@
 import './Sponsors.css'
 import blankProfile from '../../imgs/blank-profile.png'
-import {Container, Button} from 'react-bootstrap/';
+import {Container} from 'react-bootstrap/';
 
 function Sponsors(props){
     const t = props.t;
   return(
-    <Container>
+    <Container style={{textAlign: "center"}}>
         <div className='titleArea'>
-            <h1 className='sponsorsPage-title'>Supporting Our Work: A Look at Our Dedicated Sponsors</h1>
-            <h3 className='sponsorsPage-text'>Discover the valuable role of sponsors in helping us achieve our mission and learn more about the individuals and companies who make it all possible.</h3>
+            <h1 className='sponsorsPage-title'>{t('Sponsor-Us-Title')}</h1>
+            <h3 className='sponsorsPage-text'>{t('Sponsor-Us-SubTitle')}</h3>
         </div>
 
         <div class="logos">
@@ -52,7 +52,13 @@ function Sponsors(props){
                 <img src={blankProfile}alt="patrocinador" width={200}/>
             </div>
         </div>
-        <Button>{t('Sponsor-us')}</Button>
+        <button class="button-82-pushable">
+            <span class="button-82-shadow"></span>
+            <span class="button-82-edge"></span>
+            <span class="button-82-front text">
+                {t('Sponsor-us')}
+            </span>
+        </button>
     </Container>
   )
 }
