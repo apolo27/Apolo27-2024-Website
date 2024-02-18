e1f3598d648bea1197797b27e7324f5e4187404f
Luis Adames <adames1601@gmail.com>
2024-02-18 17:31:29 -0400

    Implementacion de los sensores en el codigo

--- src/pages/DataDashboard/index.js
+++ src/pages/DataDashboard/index.js
@@ -1,6 +1,6 @@
 import './DataDashboard.css'
 import React, { useEffect, useState } from 'react';
-import firebase from '../../services/firebase';
+import database from '../../services/firebase';
 import {Container,Tab, Tabs} from 'react-bootstrap';
 
 import {Grid, FormGroup, FormControlLabel, Checkbox} from '@mui/material';
@@ -22,148 +22,247 @@ const DataDashboard = (props) => {
   const [data, setData] = useState(null);
   const [processedData, setProcessedData] = useState([]);
   const [surroundingTemp, setSurroundingTemp] = useState(61);
+  const [temperatura, setTemperatura] = useState(0);
+  const [humedad, setHumedad] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         // Assuming we have a 'data' node in our database
-        const snapshot = await firebase.database()
-          .ref('/temperatura-humedad')
+        const snapshot = await database
+          .ref("/temperatura-humedad")
           .orderByKey()
           .limitToLast(1)
-          .once('value');
+          .once("value");
         const dataFromDatabase = snapshot.val();
         setData(dataFromDatabase);
-        console.log('Data from Firebase:', dataFromDatabase);
+        console.log("Data from Firebase:", dataFromDatabase);
       } catch (error) {
-        console.error('Error fetching data from Firebase:', error);
+        console.error("Error fetching data from Firebase:", error);
       }
     };
 
     fetchData();
   }, []);
 
-  // obtener los datos de la base de datos
+  // Configurar el listener 'on' para la ubicación /temperatura-humedad
   useEffect(() => {
-    if (data) {
-      const processedData = Object.entries(data).map(([key, value]) => {
-        return { key, value };
-      });
-      setProcessedData(processedData);
-    }
-  }, [data]);
-  
+    const onDataChange = (snapshot) => {
+      const dataFromDatabase = snapshot.val();
+      setData(dataFromDatabase);
+      console.log("Data from Firebase:", dataFromDatabase);
 
-  return(
+      if (dataFromDatabase) {
+        const lastEntryKey = Object.keys(dataFromDatabase)[0];
+        const lastEntryData = dataFromDatabase[lastEntryKey];
+
+        // Procesar los datos directamente
+        setProcessedData({
+          key: lastEntryKey,
+          value: lastEntryData,
+        });
+
+        // Acceder a los valores de temperatura y humedad y parsearlos como números
+        setTemperatura(parseFloat(lastEntryData.temperatura));
+        setHumedad(parseFloat(lastEntryData.humedad));
+
+        setTimeout(() => {
+          console.log('Temperatura:', temperatura.toFixed(2));
+          console.log('Humedad:', humedad.toFixed(2));
+        }, 5);
+        
+      }
+    };
+
+    const databaseRef = database.ref("/temperatura-humedad");
+    databaseRef.on("value", onDataChange);
+
+    // Devuelve una función de limpieza para desregistrar el listener cuando el componente se desmonta
+    return () => {
+      databaseRef.off("value", onDataChange);
+    };
+  }, []); // El segundo parámetro del useEffect es un array vacío para que se ejecute solo una vez al montar el componente
+
+  return (
     <Container>
-      <section className='data-dashboard-intro'>
+      <section className="data-dashboard-intro">
         <h4>
-          Cada año blablablabla
-          Este es nuestro tablero para el Nasa Herc 2024
+          Cada año blablablabla Este es nuestro tablero para el Nasa Herc 2024
         </h4>
       </section>
-      <div className='data-dashboard-body'>
-        <Tabs defaultActiveKey="overview" justify={true} variant='pills' className='mb-3'>
-          <Tab eventKey="overview" title="OVERVIEW" tabClassName='tab'>
-            <Grid container={true} wrap='wrap' justifyContent="space-evenly" rowSpacing={4}>
-
-              <Grid item xs="auto" order={{xs: 2,  md: 3, lg: 2, xl: 1 }}>
-                <div className='Task-List'>
-                  <h2 style={{fontWeight: '700'}}>{t('Tasks')}</h2>
-                  <FormGroup >
-                      <FormControlLabel control={<Checkbox sx={{color: "aliceblue", '&.Mui-checked': {color: "green"}}}/>} label={t('Find-ARV')} />
-                      <FormControlLabel control={<Checkbox sx={{color: "aliceblue", '&.Mui-checked': {color: "green"}}}/>} label={t('Regolith-removal')} />
-                      <FormControlLabel control={<Checkbox sx={{color: "aliceblue", '&.Mui-checked': {color: "green"}}}/>} label={t('Moon-maintenance')} />
-                      <FormControlLabel control={<Checkbox sx={{color: "aliceblue", '&.Mui-checked': {color: "green"}}}/>} label={t('Power-it-up')} />
-                      <FormControlLabel control={<Checkbox sx={{color: "aliceblue", '&.Mui-checked': {color: "green"}}}/>} label={t('Rover-Redundancy')}/>
+      <div className="data-dashboard-body">
+        <Tabs
+          defaultActiveKey="overview"
+          justify={true}
+          variant="pills"
+          className="mb-3"
+        >
+          <Tab eventKey="overview" title="OVERVIEW" tabClassName="tab">
+            <Grid
+              container={true}
+              wrap="wrap"
+              justifyContent="space-evenly"
+              rowSpacing={4}
+            >
+              <Grid item xs="auto" order={{ xs: 2, md: 3, lg: 2, xl: 1 }}>
+                <div className="Task-List">
+                  <h2 style={{ fontWeight: "700" }}>{t("Tasks")}</h2>
+                  <FormGroup>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          sx={{
+                            color: "aliceblue",
+                            "&.Mui-checked": { color: "green" },
+                          }}
+                        />
+                      }
+                      label={t("Find-ARV")}
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          sx={{
+                            color: "aliceblue",
+                            "&.Mui-checked": { color: "green" },
+                          }}
+                        />
+                      }
+                      label={t("Regolith-removal")}
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          sx={{
+                            color: "aliceblue",
+                            "&.Mui-checked": { color: "green" },
+                          }}
+                        />
+                      }
+                      label={t("Moon-maintenance")}
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          sx={{
+                            color: "aliceblue",
+                            "&.Mui-checked": { color: "green" },
+                          }}
+                        />
+                      }
+                      label={t("Power-it-up")}
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          sx={{
+                            color: "aliceblue",
+                            "&.Mui-checked": { color: "green" },
+                          }}
+                        />
+                      }
+                      label={t("Rover-Redundancy")}
+                    />
                   </FormGroup>
                 </div>
-                <div className='graph'>
-                  <h4 style={{fontWeight: '700'}}>{t('Rovers-Accel')}</h4>
+                <div className="graph">
+                  <h4 style={{ fontWeight: "700" }}>{t("Rovers-Accel")}</h4>
                 </div>
-              
               </Grid>
 
-              <Grid item xs="auto" order={{xs: 1, md: 1, lg: 1, xl: 2 }}>
-                <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1O7ZBN5Mw5ox-4F7-HyeIVqI7-Vc3ZG4&ehbc=2E312F&noprof=1"
-                  style={{background: <CircularProgress />}}
-                  className='herc-map'
-                  title='Nasa Herc map'
-                  id='herc-map'>
-                  
-                </iframe>
+              <Grid item xs="auto" order={{ xs: 1, md: 1, lg: 1, xl: 2 }}>
+                <iframe
+                  src="https://www.google.com/maps/d/u/0/embed?mid=1O7ZBN5Mw5ox-4F7-HyeIVqI7-Vc3ZG4&ehbc=2E312F&noprof=1"
+                  style={{ background: <CircularProgress /> }}
+                  className="herc-map"
+                  title="Nasa Herc map"
+                  id="herc-map"
+                ></iframe>
               </Grid>
 
-              <Grid item xs="auto" order={{xs: 3, md: 3, lg: 3, xl: 3 }}>
-                <div className='participants'>
-                  <h2 style={{fontWeight: '700'}}>{t('Pilots')}</h2>
-                  <List dense className='crewmembers'>
-                      <ListItem disablePadding>
-                          <ListItemButton>
-                          <ListItemAvatar>
-                              <Avatar
-                              alt="Miguel Arredondo"
-                              src="https://apolo27.com/img/about-us/team-members/miguela.png"
-                              />
-                          </ListItemAvatar>
-                          <ListItemText id={1} primary={"Miguel Arredondo"}/>
-                          </ListItemButton>
-                      </ListItem>
+              <Grid item xs="auto" order={{ xs: 3, md: 3, lg: 3, xl: 3 }}>
+                <div className="participants">
+                  <h2 style={{ fontWeight: "700" }}>{t("Pilots")}</h2>
+                  <List dense className="crewmembers">
+                    <ListItem disablePadding>
+                      <ListItemButton>
+                        <ListItemAvatar>
+                          <Avatar
+                            alt="Miguel Arredondo"
+                            src="https://apolo27.com/img/about-us/team-members/miguela.png"
+                          />
+                        </ListItemAvatar>
+                        <ListItemText id={1} primary={"Miguel Arredondo"} />
+                      </ListItemButton>
+                    </ListItem>
 
-                      <ListItem disablePadding>
-                          <ListItemButton>
-                          <ListItemAvatar>
-                              <Avatar
-                              alt="Rosanna Bautista"
-                              src="https://apolo27.com/img/about-us/team-members/rosanna.png"
-                              />
-                          </ListItemAvatar>
-                          <ListItemText id={1} primary={"Rosanna Bautista"}/>
-                          </ListItemButton>
-                      </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton>
+                        <ListItemAvatar>
+                          <Avatar
+                            alt="Rosanna Bautista"
+                            src="https://apolo27.com/img/about-us/team-members/rosanna.png"
+                          />
+                        </ListItemAvatar>
+                        <ListItemText id={1} primary={"Rosanna Bautista"} />
+                      </ListItemButton>
+                    </ListItem>
 
-                      <ListItem disablePadding>
-                          <ListItemButton>
-                          <ListItemAvatar>
-                              <Avatar
-                              alt="Raymond Ruiz"
-                              src="https://apolo27.com/img/about-us/team-members/raymond.png"
-                              />
-                          </ListItemAvatar>
-                          <ListItemText id={1} primary={"Raymond Ruiz"}/>
-                          </ListItemButton>
-                      </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton>
+                        <ListItemAvatar>
+                          <Avatar
+                            alt="Raymond Ruiz"
+                            src="https://apolo27.com/img/about-us/team-members/raymond.png"
+                          />
+                        </ListItemAvatar>
+                        <ListItemText id={1} primary={"Raymond Ruiz"} />
+                      </ListItemButton>
+                    </ListItem>
 
-                      <ListItem disablePadding>
-                          <ListItemButton>
-                          <ListItemAvatar>
-                              <Avatar
-                              alt="Ingrid Lopez"
-                              src="https://apolo27.com/img/about-us/team-members/ingrid.png"
-                              />
-                          </ListItemAvatar>
-                          <ListItemText id={1} primary={"Ingrid Lopez"} />
-                          </ListItemButton>
-                      </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton>
+                        <ListItemAvatar>
+                          <Avatar
+                            alt="Ingrid Lopez"
+                            src="https://apolo27.com/img/about-us/team-members/ingrid.png"
+                          />
+                        </ListItemAvatar>
+                        <ListItemText id={1} primary={"Ingrid Lopez"} />
+                      </ListItemButton>
+                    </ListItem>
                   </List>
                 </div>
-                <div className='environment-sneakpeek'>
-                  <h4 style={{fontWeight: '700'}}>{t('Surrounding-temp')}</h4>
-                  <div className='temperatura'>
-                    <div className='icono-temperatura'>
-                      <img alt='termometer' src={tempIcon}></img>
+                <div className="environment-sneakpeek">
+                  <h4 style={{ fontWeight: "700" }}>{t("Surrounding-temp")}</h4>
+                  <div className="temperatura">
+                    <div className="icono-temperatura">
+                      <img alt="termometer" src={tempIcon}></img>
                     </div>
-                    <h3 >{surroundingTemp}<span style={{fontSize: '14px', paddingBottom: "15px"}}>°F</span></h3>
+                    <h3>
+                      {temperatura ? temperatura : surroundingTemp}
+                      <span style={{ fontSize: "14px", paddingBottom: "15px" }}>
+                        °C
+                      </span>
+                    </h3>
                   </div>
                 </div>
               </Grid>
             </Grid>
           </Tab>
 
-          <Tab eventKey="rover" title="ROVER" tabClassName='tab'>
-            <Canvas spr={[1,2]} camera={{fov: 45}} style={{width: "100%", height: "720px"}}>
-              <color attach="background" args={["#283b66"]}/>
-              <PresentationControls speed={1.5} zoom={.5} polar={[-0.1, Math.PI/4]}>
+          <Tab eventKey="rover" title="ROVER" tabClassName="tab">
+            <Canvas
+              spr={[1, 2]}
+              camera={{ fov: 45 }}
+              style={{ width: "100%", height: "720px" }}
+            >
+              <color attach="background" args={["#283b66"]} />
+              <PresentationControls
+                speed={1.5}
+                zoom={0.5}
+                polar={[-0.1, Math.PI / 4]}
+              >
                 <Stage environment={null}>
                   <Model scale={0.01} />
                 </Stage>
@@ -195,9 +294,8 @@ const DataDashboard = (props) => {
       </tbody>
       </Table>
       */}
-
     </Container>
-  )
+  );
 }
 
 export default DataDashboard;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -30,5 +30,6 @@ firebase.auth().signInWithEmailAndPassword(user, password)
   });
 
 export const auth = firebase.auth();
+const database = firebase.database();
 
-export default firebase;
+export default database;
