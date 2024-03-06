d347e5c38da1683d8d793ce66c58af22d9d21d4f
Rosanna Bautista <rosannaminyety@gmail.com>
2024-03-05 23:12:50 -0400

    Header update responsive*

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,95 @@
+/* global jQuery */
+
 import './Header.css'
-import React from 'react'
+import $ from 'jquery'; // Import jQuery
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import logo from '../../imgs/logo.svg'
-import {Container, Nav, Navbar, Button} from 'react-bootstrap/';
-import Themes from '../Themes';
-import SelectLanguage from '../SelectLanguage';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap/';
 
-function Header(props){
-  let t = props.t;
+function Header(props) {
+    let t = props.t;
 
-  return(
-    <div>
-      <Navbar className='titulo' bg="dark" data-bs-theme="dark" expand="lg">
-        <Container className="header">
-        <Navbar.Brand href="/"><img src={logo} width={75} alt='logo' /></Navbar.Brand>
+    useEffect(() => {
 
-          <div className='links'>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href='/About-Us'>{t('ABOUT-THE-TEAM')}</Nav.Link>
-                <Nav.Link href="/Data-Dashboard">{t('DATA-DASHBOARD')}</Nav.Link>
-                <Nav.Link href="/Stem-With-Us">{t('STEM-WITH-US')}</Nav.Link>
-                <Nav.Link href="/Sponsors">{t('BECOME-A-SPONSOR')}</Nav.Link>
-                {
-                  window.screen.width >= 1280 ? <Nav.Link href="/Games">{t('GAMES')}</Nav.Link> : <></>
-                }
-              </Nav>
-            <Themes />
-            <SelectLanguage />
-            </Navbar.Collapse>
-          </div>
-        </Container>
-        </Navbar>
-    </div>
-  )
+        function test() {
+            var tabsNewAnim = $('#navbarSupportedContent');
+            var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+            var activeItemNewAnim = tabsNewAnim.find('.active');
+            var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+            var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+            var itemPosNewAnimTop = activeItemNewAnim.position();
+            var itemPosNewAnimLeft = activeItemNewAnim.position();
+            $(".hori-selector").css({
+                "top": itemPosNewAnimTop.top + "px",
+                "left": itemPosNewAnimLeft.left + "px",
+                "height": activeWidthNewAnimHeight + "px",
+                "width": activeWidthNewAnimWidth + "px"
+            });
+            $("#navbarSupportedContent").on("click", "li", function (e) {
+                $('#navbarSupportedContent ul li').removeClass("active");
+                $(this).addClass('active');
+                var activeWidthNewAnimHeight = $(this).innerHeight();
+                var activeWidthNewAnimWidth = $(this).innerWidth();
+                var itemPosNewAnimTop = $(this).position();
+                var itemPosNewAnimLeft = $(this).position();
+                $(".hori-selector").css({
+                    "top": itemPosNewAnimTop.top + "px",
+                    "left": itemPosNewAnimLeft.left + "px",
+                    "height": activeWidthNewAnimHeight + "px",
+                    "width": activeWidthNewAnimWidth + "px"
+                });
+            });
+        }
+        $(document).ready(function () {
+            setTimeout(function () { test(); });
+        });
+        $(window).on('resize', function () {
+            setTimeout(function () { test(); }, 500);
+        });
+        $(".navbar-toggler").click(function () {
+            $(".navbar-collapse").slideToggle(300);
+            setTimeout(function () { test(); });
+        });
+
+        jQuery(document).ready(function ($) {
+            var path = window.location.pathname.split("/").pop();
+            if (path == '') {
+                path = 'index.html';
+            }
+            var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
+            target.parent().addClass('active');
+        });
+    }, []);
+
+    return (
+        <nav className="navbar navbar-expand-custom navbar-mainbg">
+            <a className="navbar-brand navbar-logo" href="/"></a>
+            <a className="navbar-brand navbar-logo" href="/">
+            <img src={logo} alt="Logo" />
+        </a>
+            <button className="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i className="fas fa-bars text-white"></i>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <div className="hori-selector"><div className="left"></div><div className="right"></div></div>
+                    <li className="nav-item active">
+                        <Link to="/" className="nav-link"><i className=""></i>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/Data-Dashboard" className="nav-link"><i className=""></i>Dashboard</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/Stem-With-Us" className="nav-link"><i className=""></i>STEM With Us</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/Sponsors" className="nav-link"><i className=""></i>Become a Sponsor</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    ) 
 }
 
 export default Header;
