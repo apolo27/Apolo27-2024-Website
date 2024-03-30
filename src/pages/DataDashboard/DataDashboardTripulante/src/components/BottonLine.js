94d07ad00c64a17db00d44007c59acd9a0e0cd16
Luis Adames <adames1601@gmail.com>
2024-03-30 19:13:56 -0400

    arreglo responsive crewmembers

--- src/pages/DataDashboard/DataDashboardTripulante/src/components/BottonLine.js
+++ src/pages/DataDashboard/DataDashboardTripulante/src/components/BottonLine.js
@@ -1,10 +1,31 @@
 import ActivityCrewMember from "./ActivityCrewMember";
 import styles from "./BottonLine.module.css";
 import vector2 from "../../public/vector-2.svg";
+import React, { useState, useEffect } from 'react';
+import { CContainer, CRow, CCol, CWidgetStatsF, CWidgetStatsA,
+  CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle
+  } from '@coreui/react'
 
 const BottonLine = () => {
+
+  // Estado local para almacenar la fecha actual
+  const [currentDate, setCurrentDate] = useState(new Date());
+  
+
+  useEffect(() => {
+    // Función que se ejecuta cada segundo para actualizar la fecha
+    const timer = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    // Limpiar el temporizador
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <section className={styles.listHandler}>
+    <CContainer style={{maxWidth:'auto'}}>
       <div className={styles.graphFormer}>
         <div className={styles.mapCreator}>
           <div className={styles.algorithmEngine}>
@@ -14,22 +35,16 @@ const BottonLine = () => {
                   <h1 className={styles.saludDelTripulante}>
                     Salud Del Tripulante
                   </h1>
-                  <div className={styles.august122023}>August 12, 2023</div>
+                  <div className={styles.august122023}>{currentDate.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}</div>
                 </div>
-              </div>
-              <div className={styles.loopController}>
-                <img className={styles.descendantOnceRemovedNodes} alt="" />
-                <img className={styles.descendantOnceRemovedNodes1} alt="" />
               </div>
             </div>
           </div>
-          <button className={styles.locationbutton}>
-            <img className={styles.filterNodeIcon} alt="" src={vector2} />
-          </button>
         </div>
       </div>
+      
       <ActivityCrewMember />
-    </section>
+    </CContainer>
   );
 };
 
